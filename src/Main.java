7392bbf913c380efb3debf92f892720d97abc575
GIDRO32 <pambossable@gmail.com>
2022-12-29 12:48:58 +0200

    Add project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int[] list = {143,415,21,56,24,2552,152,25,652};
+        for(int i:list)
+        {
+            System.out.println(i);
+        }
     }
 }
